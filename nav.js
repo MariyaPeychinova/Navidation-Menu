5c390bc7926b41474c7f4fb683702ab5f2e6de05
Mariya Peychinova <mariapeichinova@gmail.com>
2022-02-13 22:34:26 +0000

    First commit

---
+++ nav.js
@@ -0,0 +1,24 @@
+function navSlide() {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    function openNavLinks() {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+        
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Hamburger Animation
+       // hamburger.classList.toggle('toggle');
+    }
+    hamburger.addEventListener('click', openNavLinks);
+}
+
+navSlide();
